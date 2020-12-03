7aacc99cdfda53def4416621e4b0bb35b0d73380
305675920@qq.com <305675920@qq.com>
2020-12-03 17:10:35 +0800

    组件传值bug 👌

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,10 @@
         <div>
             <h3>天气展示</h3>
             <p v-if="states.city" class="weather">
-                你搜索城市是<span>{{ states.city }}</span>, 今天是<span>{{ states.date
-                }}</span><span>{{ states.week }}</span>, 天气<span>{{ states.wea }}</span>,<span>{{ states.win
-                }}</span><span>{{ states.win_speed }}</span>, 风速<span>{{ states.win_meter }}</span>, 
+                你搜索城市是<span>{{ states.city }}</span>, 
+                今天是<span>{{ states.date}}</span><span>{{ states.week }}</span>, 
+                天气<span>{{ states.wea }}</span>,<span>{{ states.win}}</span><span>{{ states.win_speed }}</span>, 
+                风速<span>{{ states.win_meter }}</span>, 
                 空气<span>{{states.air_level}}</span>,pm2.5:<span>{{ states.air }}</span>,<br>
                 <b><span>{{ states.air_tips }}</span></b>
             </p>
@@ -60,6 +61,7 @@ export default {
         const { getDatas, reqcity, setVuex, states } = weatherHooks(reqcity);
         function showModal() {
             visible.value = true;
+            str.value = "父组件打开文字"
         }
         function hideModal() {
             message.success("点击了确定");
